581221aabb4b185781d595e8e51aa25c7710a53e
adey-px <aosteph@gmail.com>
2022-07-08 15:12:36 -0500

    Added profile model and signals

--- real_estate/urls.py
+++ real_estate/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 
+# Create urls for apps, first is site admin login url
 urlpatterns = [
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
