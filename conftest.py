1a4f13af3b275aefc643e17082fb81086d631086
adey-px <aosteph@gmail.com>
2022-07-18 11:05:59 -0500

    Added custom user/profle model tests

---
+++ conftest.py
@@ -0,0 +1,28 @@
+import pytest
+from pytest_factoryboy import register
+
+from tests.factories import ProfileFactory, UserFactory
+
+
+# Register model factories
+register(ProfileFactory)
+register(UserFactory)
+
+
+# Declare fixtures to run
+@pytest.fixture
+def base_user(db, user_factory):
+    new_user = user_factory.create()
+    return new_user
+
+
+@pytest.fixture
+def super_user(db, user_factory):
+    new_user = user_factory.create(is_staff=True, is_superuser=True)
+    return new_user
+
+
+@pytest.fixture
+def profile(db, profile_factory):
+    user_profile = profile_factory.create()
+    return user_profile
